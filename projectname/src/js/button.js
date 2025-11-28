92c4d4cd6a3086962e2cbb2411e482256bfceb1b
Angeleena Livingston D <angeleenalivingston@gmail.com>
2025-11-28 16:58:05 +0530

    JS

---
+++ projectname/src/js/button.js
@@ -0,0 +1,8 @@
+
+
+
+document.getElementById("button").addEventListener("click",function(){
+ document.getElementById('Display').style.backgroundColor='Black';
+}
+)
+
---
+++ projectname/src/js/fact.js
@@ -0,0 +1,10 @@
+n=prompt("ENTER A NO. TO GET THE FACTORIAL:")
+let factorial=1;
+
+
+for( i=1;i<=n;i++)
+{
+    factorial*=i
+    
+}
+console.log( 'factorial of given number'+ n+ 'is  '+ factorial)
